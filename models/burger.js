8ad446eaf39c30b75eb73dcc33f5bdda83284d70
JuliusDorfman <juliusdorfman@gmail.com>
2017-11-18 14:40:10 -0800

    created songs table via sequelize

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,20 @@
 // Burger models
-
 // The burger has a burger_name attribute of type DataTypes.String
 // and a devoured attribute that is false by default
 
 module.exports = function(sequelize, DataTypes) {
-  var Burger = sequelize.define("Burger", {
-    burger_name: DataTypes.STRING,
-    devoured: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    }
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return Burger;
-};
+    var Song = sequelize.define("Song", {
+        id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
+        song_name: { type: DataTypes.STRING },
+        valence: { type: DataTypes.INTEGER },
+        liveliness: { type: DataTypes.INTEGER },
+        energy: { type: DataTypes.INTEGER }
+    }, {
+        classMethods: {
+            associate: function(models) {
+                // associations can be defined here
+            }
+        }
+    });
+    return Song;
+}
