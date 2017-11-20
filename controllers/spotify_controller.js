4da9fa9b1a0667ecaf6ccedea304d4751d99c2f6
JuliusDorfman <juliusdorfman@gmail.com>
2017-11-19 21:45:56 -0800

    created constructor that will contain all necessary song info to pass to front end

--- controllers/spotify_controller.js
+++ controllers/spotify_controller.js
@@ -7,21 +7,31 @@ var spotify = new Spotify({
     secret: keys.spotifyKeys.client_secret
 });
 
+
+function Song(song_name, valence, liveliness, energy, duration) {
+    this.song_name = song_name,
+        this.valence = valence,
+        this.liveliness = liveliness,
+        this.energy = energy,
+        this.duration = duration
+}
+
 //Spotify Exercise
 var spotifyRouter = {
     spotifyThisSong: function(trackName = "Happy McFarren", cb) {
-        
+
         spotify.search({ type: 'track', query: trackName, limit: 5 }, function(err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
+
             var firstResult = data.tracks.items[0];
-            var songId = firstResult.id;
-            var oaToken = 'BQC8tNZ8MYuU6CzW3RN8_3WaiY2dBcmfiT3Ov456T0fwG2dx_bGZLoQr06SVoRh7TTYSTQ1RQzByeEOEfUBTWNisVLJVROnEz7MNi_j8P5GH850--rNYHAyFiNW2u2JY51-ozotAMP-Z19qO_jN6';
+            var songIdNum = firstResult.id;
+            var oaToken = 'BQCOOs17vXUnXORt6eocvABkyV6SmwTQjEb4ba42lAz005RlJbevTWvRPYbseO_ySIlphBV_Tc8XEBcvoNPkzPjmNyhoOcl5hsUR65DV_GbwE5vm_xnGlnDXJQ1YSNTr8uPOXBVjTUY';
 
             var options = {
                 method: 'GET',
-                url: 'https://api.spotify.com/v1/audio-features/' + songId,
+                url: 'https://api.spotify.com/v1/audio-features/' + songIdNum,
                 headers: {
                     'cache-control': 'no-cache',
                     authorization: 'Bearer ' + oaToken,
@@ -33,10 +43,30 @@ var spotifyRouter = {
             request(options, function(error, response, body) {
                 if (error) throw new Error(error);
 
-                console.log("Valence: ", body.valence);
-                console.log("Liveness: ", body.liveness);
-                console.log("Energy: ", body.energy);
-                console.log("SongId: ", songId);
+                var song_name = firstResult.name
+                var valence = body.valence;
+                var liveness = body.liveness;
+                var energy = body.energy;
+                var songId = songId;
+                var duration_ms = firstResult.duration_ms;
+                console.log('FIRST Result', firstResult.duration_ms);
+                var songObject = new Song(
+                    song_name,
+                    valence,
+                    liveness,
+                    energy,
+                    songId,
+                    // TODO: FOR some reason, unable to log the duration in here
+                    duration_ms
+                )
+
+                console.log('SONG OBJECT', songObject);
+
+                //TODO: Are the console.logs below now redundant?
+                console.log("Valence: ", valence);
+                console.log("Liveness: ", liveness);
+                console.log("Energy: ", energy);
+                console.log("SongId: ", songIdNum);
                 console.log("Duration in ms: ", firstResult.duration_ms);
                 //Todo wrap callback into object - get help!
                 cb(body.valence);
@@ -47,7 +77,7 @@ var spotifyRouter = {
                 "* Artist(s): " + firstResult.album.artists[0].name +
                 "* Preview Link: " + firstResult.external_urls.spotify +
                 "* Album Name: " + firstResult.album.name +
-                "* Song ID: " + songId;
+                "* Song ID: " + songIdNum;
             var dataArr = trackInfo.split("*");
             for (i = 0; i < dataArr.length; i++) {
                 console.log(dataArr[i].trim());
@@ -56,6 +86,7 @@ var spotifyRouter = {
 
         });
     }
+
 } //End Spotify Exercise
 
 module.exports = spotifyRouter;
