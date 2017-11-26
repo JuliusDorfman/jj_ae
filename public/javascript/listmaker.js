cc7dbc13d27ff2a3a1a03a330ca213c8f9c474d0
Joe <jholland132465@gmail.com>
2017-11-25 17:33:32 -0800

    tweaked buttons

--- public/javascript/listmaker.js
+++ public/javascript/listmaker.js
@@ -1,10 +1,2 @@
-$.ajax({
-    url: queryURL,
-    method: "GET",
-    dataType: 'json',
-}).done(function(response) {
-    console.log(response);
-   
-$("#moodimage").prepend('<img id="unicorn" src="assets/images/uni.jpg">');
 
-	$('#theDiv').prepend('<img id="theImg" src="theImg.png" />')
+
