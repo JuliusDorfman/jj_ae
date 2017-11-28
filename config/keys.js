6ba6c8976b1835a7fad55765f89ba6c172a906fc
Enrico Bernardo <ebernardo@gmail.com>
2017-11-27 18:31:00 -0800

    heroku

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 exports.spotifyKeys = {
 	client_id: '52b4253865e24076a3bbb8838d4ce261',
-	// client_secret: '783a365ecebe4cfcb9fed0e7f5d2f484',
+	client_secret: '783a365ecebe4cfcb9fed0e7f5d2f484',
 	redirect_uri: 'https://moodmusic1.herokuapp.com/callback'
-	redirect_uri: 'http://localhost:3000/callback'
+	// redirect_uri: 'http://localhost:3000/callback'
 };
